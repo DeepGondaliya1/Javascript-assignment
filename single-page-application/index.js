2b225aa5f61ca501ba1c0a329ae0d1aa87c0d440
deepgondaliya1 <Deep@techeniac.com>
2025-02-16 19:36:33 +0530

    add smooth scrolling

--- single-page-application/index.js
+++ single-page-application/index.js
@@ -1,9 +1,9 @@
 const backgroundImage = [
-  `url('https://images.pexels.com/photos/3845077/pexels-photo-3845077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
-  `url('https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
-  `url('https://images.pexels.com/photos/1457812/pexels-photo-1457812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
-  `url('https://images.pexels.com/photos/3482719/pexels-photo-3482719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
-  `url('https://i.ytimg.com/vi/XOxJwTxrIX4/maxresdefault.jpg')`,
+  `url('https://img.freepik.com/free-vector/egyptian-desert-with-river-pyramids-night_107791-4618.jpg?t=st=1739713719~exp=1739717319~hmac=608e037fee8b277cf805b44de82597e0e331681d31a70f46a11cec6dcbec4957&w=1380')`,
+  `url('https://img.freepik.com/free-vector/night-ocean-landscape-full-moon-stars-shine_107791-7397.jpg?t=st=1739713401~exp=1739717001~hmac=1088907190ea74b5a80e11924093849c462b217ed9e86944b46b9a78122408e0&w=1380')`,
+  `url('https://img.freepik.com/free-vector/wild-desert-landscape-night-scene_1308-52214.jpg?t=st=1739713862~exp=1739717462~hmac=26b14b690568cd8bc1d1ff1199aab767893935ff4e490378da4d2bd0fb1f8494&w=1380')`,
+  `url('https://img.freepik.com/free-vector/bedouins-walk-egypt-pyramids-camel-night-desert_107791-4619.jpg?t=st=1739713754~exp=1739717354~hmac=8dd78ea4381eae0e09b723ce511e4d0f5a127effc400cf167fd70b77012df74c&w=1380')`,
+  `url('https://img.freepik.com/premium-photo/dream-desktop-wallpaper_941097-164233.jpg?w=1060')`,
 ];
 
 let currentImage = 0;
@@ -28,3 +28,14 @@ document.getElementById("right-slider").addEventListener("click", () => {
 });
 
 updateImage();
+
+
+// smooth scrolling for navigation 
+
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const targetId = link.getAttribute("href").substring(1);
+    document.getElementById(targetId).scrollIntoView({behavior: "smooth"});
+  });
+});
